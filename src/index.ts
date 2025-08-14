de3d4ca9fb59257cbdd422373a447827bbfec64a
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 11:47:01 -0700

    Add index file to export DuplicateAndRename component

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Sanity Duplicate and Rename - Main Export
+ * Duplicate documents with customizable naming patterns and field updates
+ */
+
+export { default as DuplicateAndRename } from './DuplicateAndRename'
+export { default } from './DuplicateAndRename'
+
+// Export types if needed
+export type {
+  DuplicateAndRenameProps,
+  DuplicationResult
+} from './DuplicateAndRename'
